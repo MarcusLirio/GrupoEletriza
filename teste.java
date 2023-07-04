fbd632113b957517c38c9ee49c4ad5c593547de5
MarcusLirio <marcus.lirio1@gmail.com>
2023-07-03 23:17:18 -0300

    Aula dos meninos

---
+++ teste.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String html = "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    <title>Document</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "    <h1>Helloworld</h1>\n" +
+                "</body>\n" +
+                "</html>";
+
+        System.out.println(html);
+    }
+}
